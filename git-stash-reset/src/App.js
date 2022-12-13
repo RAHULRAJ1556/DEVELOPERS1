acd4a8db556e7815b2a4c698337600ac5dc615f5
RAHULRAJ <rahulrajkb1556@gmail.com>
2022-12-13 20:51:33 +0530

    added great leaders headings

--- git-stash-reset/src/App.js
+++ git-stash-reset/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<h1>GREAT FREEDOM FIGHTTERS</h1> 
+<h2>Dr. B R Ambedkar</h2>
+<h2>Dr. Rajendra Prasad</h2>
+<h2>Bhagat Singh</h2>
+<h2>Alluri Sitarama Raju</h2>
+<h2>Subhas Chandra Bose</h2>
+<h2>Mohandas Karamchand Gandhi</h2>
+</div>
+);
 }
 
 export default App;
